5ad3a8a229c0044fec8f0a2139599035c58a2302
chellemo <chellemo23@gmail.com>
2019-06-28 00:21:04 -0400

    updating

--- models/cookie.js
+++ models/cookie.js
@@ -21,7 +21,7 @@ module.exports = (sequelize, DataTypes) => {
             type: DataTypes.STRING,
             allowNull: false
         },
-        submittedby: {
+        submittedBy: {
             type: DataTypes.STRING,
             allowNull: false
         }
