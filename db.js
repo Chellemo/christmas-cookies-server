cb0014581a86156f4827cd5a70fb8d1c50f31252
Michelle Smith <chellemo23@gmail.com>
2019-08-09 22:05:53 -0400

    Adding to github repository

--- db.js
+++ db.js
@@ -1,8 +1,8 @@
 const Sequelize = require('sequelize');
 const sequelize = new Sequelize(process.env.DATABASE_URL, {
-
+    host: 'localhost',
     dialect: 'postgres'
-});
+  });
 
 sequelize.authenticate()
     .then(() => console.log('pern-stack-server db is connected'))
